22dac091e05a4c0ce1cc537b0c9cd24e3ecb55cc
Konstantin Dvornichenko <knstd@rambler.ru>
2023-04-17 19:06:32 +0400

    исправлена ошибка

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,17 +3,16 @@ let tabContent = Array.from(document.querySelectorAll('.tab__content'))
 
 
 for (i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', tabClick)
+    const tab = tabs[i]
+    tab.addEventListener('click', tabClick)
 
-    function tabClick(tab) {
-        for (let i = 0; i < tabs.length; i++) {
-            let tabIndex = tabs.indexOf(tab.target)
-            let tabActive = document.querySelector('.tab_active')
-            let tabConentActive = document.querySelector('.tab__content_active')
-            tabActive.classList.remove('tab_active')
-            tabConentActive.classList.remove('tab__content_active')
-            tabs[i].classList.add('tab_active')
-            tabContent[tabIndex].classList.add('tab__content_active')
-        }
+    function tabClick() {
+        let tabIndex = tabs.indexOf(tab)
+        let tabActive = document.querySelector('.tab_active')
+        let tabConentActive = document.querySelector('.tab__content_active')
+        tabActive.classList.remove('tab_active')
+        tabConentActive.classList.remove('tab__content_active')
+        tab.classList.add('tab_active')
+        tabContent[tabIndex].classList.add('tab__content_active')
     }
 }
